660c1b0b58eab6b20d63ee56a182329c47b22655
fasikakassahun <fasikakassahun1912@gmail.com>
2026-02-07 12:04:46 +0300

    simple user managemnt system

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
-print("BIZUAYEHU SIMACHEW DUBALE")
-print("""PS C:\Users\buze5\Desktop\python> & C:/Users/buze5/AppData/Local/Programs/Python/Python313/python.exe
-Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license" for more information.
-Ctrl click to launch VS Code Native REPL
->>> print("BIZUAYEHU SIMACHEW DUBALE")
-BIZUAYEHU SIMACHEW DUBALE
->>> """)
+class EmployeeDatabase:
+    def __init__(self,name,age,department,id,salary):
+        self.name = name
+        self.age = age
+        self.department = department
+        self.id = id
+        self.__salary = salary
+        self.employees = []
+        
+    def get_employee_data(self,ID):
+        if not isinstance(ID,str):
+            raise ValueError("ID must be a string")
+        if not isinstance(self.age,int):
+            raise ValueError("Age must be an integer")
+        self.employees.append({
+            "id": self.id,
+            "name": self.name,
+            "age": self.age,
+            "department": self.department,
+            "__salary": self.__salary
+        })
+        if ID in [emp["id"] for emp in self.employees]:
+        
+            return {
+                "name": self.name,
+                "age": self.age,
+                "department": self.department,
+                "ID": self.id,
+                "salary": self.__salary
+            }
+            
+        else:
+            raise ValueError(f"Employee with ID {self.id} does not exist")
+            
+            
+employee = EmployeeDatabase("Buze",27,"SOFTWARE","buz123",400000)
+
+print(employee.get_employee_data("buz123"))
