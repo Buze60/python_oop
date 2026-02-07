4d6f4ef360cfde5ea9a07c2944f17f21ad09a50a
fasikakassahun <fasikakassahun1912@gmail.com>
2026-02-07 11:38:09 +0300

    test commet

---
+++ main.py
@@ -0,0 +1,8 @@
+print("BIZUAYEHU SIMACHEW DUBALE")
+print("""PS C:\Users\buze5\Desktop\python> & C:/Users/buze5/AppData/Local/Programs/Python/Python313/python.exe
+Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+Ctrl click to launch VS Code Native REPL
+>>> print("BIZUAYEHU SIMACHEW DUBALE")
+BIZUAYEHU SIMACHEW DUBALE
+>>> """)
