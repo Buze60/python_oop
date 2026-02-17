2fbc3c99600425250c4c4f9df19e59c534091efd
fasikakassahun <fasikakassahun1912@gmail.com>
2026-02-17 12:49:23 +0300

    multiple Inheritance

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-class Animal:
-    def __init__(self, name):
+print("================================Multiple Inheritance================================")
+class Parent:
+    
+    pre_conditions = {
+        "religious": "Orthodox christian",
+        "Nationality":"Ethiopian",
+        "Family Members":"Simachew Dubale's Family meember"
+    }
+    def __init__(self,name):
         self.name = name
+        
 
-    def sound(self):
-        return f'{self.name} makes a sound woof!'
+    def displayPreconditions(self):
+        for key,value in self.pre_conditions.items():
+            familyMeneber = input(f"Is the child {key}: {value}? (yes/no) ").lower()
+            if familyMeneber.lower() == "no":
+                print(f"The child is not {value} {key}")
+            
+            if familyMeneber.lower() == "yes":
+                print(f"the child is {value}")
+                
+        
+        
+class Wealth(Parent):
+    pass
+    
+class childTwo(Parent,Warning):
+    pass
+
+
+
+child = childTwo("Ali")
+print(child.displayPreconditions())
+   
+   
+
     
 
-class Dog(Animal):
-    
-    def sound(self):
-        base = super().sound()
-        return f"{base} and {self.name} is eating meet"
 
-animal1 = Dog("Jack")
-print(animal1.sound())
