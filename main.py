3c49bc53778d710ffebc73da7ce54d7421669a55
fasikakassahun <fasikakassahun1912@gmail.com>
2026-02-20 13:07:22 +0300

    inheritance-based polymorphisim

--- main.py
+++ main.py
@@ -1,35 +1,19 @@
-class tweet:
-    def __init__(self,content):
-        self.content = content
-        
-    def post(self):
-        return f"Tweet posted: {self.content}"
-        
-class Instagram:
-    def __init__(self,content):
-        self.content = content
-        
-    def post(self):
-        
-        return f"Instagram post: {self.content}"
+class Animal:
+    def speak(self):
+        return "I am an animal"
+class Cat(Animal):
+    def speak(self):
+        return "Meow!"
+class Dog(Animal):
+    def speak(self):
+        return "Woof! woof!!"
+class Cow(Animal):
+    def speak(self):
+        return "Moo! Moo!! moo!!!"
     
-class Facebaook:
-    def __init__(self,content):
-        self.content = content
-        
-    def post(self):
-        return f"Facebook post: {self.content}"
+def sound(animal):
+    return animal.speak()
     
-    
-    
-    
-def social_media_post(post):
-    return post.post()
-
-tweet_post = tweet("Elon Musk said 'money can't buy happiness'")
-instagram_post = Instagram("the way to get started is to quit talking and begin doing")
-facebook_post = Facebaook("The only way to do great work is to love what you do")
-
-print(social_media_post(tweet_post))
-print(social_media_post(instagram_post))
-print(social_media_post(facebook_post))
+print(sound(Cat()))
+print(sound(Dog()))
+print(sound(Cow()))
